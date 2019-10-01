ea4e1751f505cf86a4a8b3960d3a69576ce3db0e
BaoHuynh389 <54543807+BaoHuynh389@users.noreply.github.com>
2019-10-01 14:10:57 -0600

    Create v0.6.js

---
+++ v0.6.js
@@ -0,0 +1,11 @@
+input.onGesture(Gesture.Shake, function () {
+    for (let i = 0; i < 0 + 0; i++) {
+        for (let index = 0; index <= 4; index++) {
+            if (true) {
+                index = 0
+            } else {
+                index += 1
+            }
+        }
+    }
+})
