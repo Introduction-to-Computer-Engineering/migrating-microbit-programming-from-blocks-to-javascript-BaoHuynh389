52f724f3eed6048075e12eddc447722f1b3f3384
BaoHuynh389 <54543807+BaoHuynh389@users.noreply.github.com>
2019-10-01 14:11:58 -0600

    Create v0.9.js

---
+++ v0.9.js
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    let change_PlayersTie = 0
+    index = 0
+    basic.showString("" + change_PlayersTie)
+    index = 0
+})
+input.onButtonPressed(Button.B, function () {
+    index = 0
+    basic.showString("" + index)
+    index = 0
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(0)
+})
+let index = 0
+index = 0
+index = 0
